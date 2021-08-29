3af61fbe7f82f424a942afb2066d876812c8d9d2
Your Namequevinmartinezbotina@gmail.com <you@example.com>
2021-08-29 17:55:22 -0500

    Correction conection db

---
+++ api/conect.Example.php
@@ -0,0 +1,4 @@
+$bd = "namedb";
+$host = "mysql:host=localhost:3306;charset=utf8;dbname=" . $bd;
+$usuario = "user";
+$password = "password";
--- api/conexion.php
+++ api/conexion.php
@@ -2,13 +2,9 @@
 
 class Conexion
 {
-
     public function conectar()
     {
-        $bd = "crudphp";
-        $host = "mysql:host=localhost:3306;charset=utf8;dbname=" . $bd;
-        $usuario = "root";
-        $password = "";
+        include 'conect.php';
         $link = new PDO($host, $usuario, $password, array(
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
         ));
